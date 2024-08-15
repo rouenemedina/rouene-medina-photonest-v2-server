32cc82e7281da5503b004361cd001001b7c1d332
rouenemedina <rouenemedina@gmail.com>
2024-08-15 13:50:32 -0400

    installed cors package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express, {Application} from "express";
 import "dotenv/config";
+import cors from "cors";
+import authRoutes from "./routes/auth.ts";
 
 const app: Application = express();
 const PORT: number = parseInt(process.env.PORT as string) || 8081;
+
+app.use(cors());
+app.use(express.json());
+
+//Routes:
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
