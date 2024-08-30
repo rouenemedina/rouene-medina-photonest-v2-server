a0276e65c9fbe3f622aa73d38502e8e7b8b239d3
rouenemedina <rouenemedina@gmail.com>
2024-08-30 02:06:23 -0400

    removed comments from hero-controller, and created the featured controller and route, for testing

--- src/controllers/featured-controller.ts
+++ src/controllers/featured-controller.ts
@@ -1,22 +1,22 @@
 import initKnex from "knex";
-import configuration from "../configurations/knexfile.ts";
+import configuration from "configurations/knexfile";
 import "dotenv/config";
 import { Request, Response } from "express";
-import { uploadImgHandler } from "./uploadImg-controller.ts";
+import { uploadImgHandler } from "./uploadImg-controller";
 
 const knex = initKnex(configuration);
 
 //POST /upload
-const uploadHero = async (req: Request, res: Response) => {
-  const table_name = "hero";
+const uploadFeatured = async (req: Request, res: Response) => {
+  const table_name = "featured";
   await uploadImgHandler(table_name)(req, res);
 };
 
-//GET /hero/:userId
-const heroIndex = async (req: Request, res: Response) => {
+//GET /featured/:userId
+const featuredIndex = async (req: Request, res: Response) => {
   try {
     const { user_id } = req.params;
-    const response = await knex("hero").where({ user_id }).first();
+    const response = await knex("featured").where({ user_id }).first();
     res.status(200).json(response);
   } catch (err) {
     console.log(err);
@@ -27,4 +27,4 @@ const heroIndex = async (req: Request, res: Response) => {
   }
 };
 
-export { uploadHero, heroIndex };
+export { uploadFeatured, featuredIndex };
---
+++ src/routes/featured.ts
@@ -0,0 +1,13 @@
+import express, { Router } from "express";
+import multer from "multer";
+import * as featuredController from "../controllers/featured-controller.ts";
+
+const router: Router = express.Router();
+const upload = multer({ dest: "uploads/" });
+
+router.route("/uplaod")
+  .post(upload.array("files", 10), featuredController.uploadFeatured);
+
+router.route("/:userId").get(featuredController.featuredIndex);
+
+export default router;
